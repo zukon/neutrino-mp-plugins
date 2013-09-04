6ada865da842562d2d99267bb015528730fb5a3c
TangoCash <me@spamdot.com>
2013-09-04 21:42:14 +0200

    fix tab in shellexec (thx DBoxOldie)

--- shellexec/text.c
+++ shellexec/text.c
@@ -630,7 +630,9 @@ void RenderString(char *string, int sx, int sy, int maxwidth, int layout, int si
 						{
 							sx=((sx/TABULATOR)+1)*TABULATOR;
 						}
+					break;
 				}
+				++rptr;
 			}
 			else
 			{
